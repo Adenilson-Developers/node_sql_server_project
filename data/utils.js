060cab5c4825f40b81a6d1c931731400175361be
Adenilson-Developers <adenilson.developers@gmail.com>
2022-12-27 18:25:06 -0300

    criando util data

---
+++ data/utils.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fs = require('fs-extra');
+const { json } = require('path');
+
+createEvent
+
+const loadSQLQueries = async (fileName) => {
+    const filePath = json(process.cwd(), 'data', fileName);
+    const file = fs.readdir(filePath);
+    const sqlFiles = await file.filter( f => f.endsWith('.sql'));
+    const queries = {}
+
+    for(const slqfile of sqlFiles){
+        const query = await fs.readFileSync(join(filePath, sqlFiles), { encoding: 'UTF-8'});
+        queries[slqfile.replace(".sql", "")] = query
+    }
+    return queries;
+
+}
+
+module.exports = {
+    loadSQLQueries
+}
