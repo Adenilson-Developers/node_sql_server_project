9cbf899a10b443df4492e682778ad208281b4fd3
Adenilson-Developers <adenilson.developers@gmail.com>
2023-01-02 15:08:02 -0300

    passando o router para o server

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const cors = require('cors');
 const config = require('./config');
 const bodyParser = require('body-parser');
 
+const eventRouter = require('./routers/eventRouter');
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api', eventRouter.routes);
 
 app.listen(config.port, () =>  console.log('Server is running on http://localhost:' + config.port));
