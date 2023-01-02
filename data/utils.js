f69a00b07592df125f9dfb606e5417f8d419a546
Adenilson-Developers <adenilson.developers@gmail.com>
2023-01-02 16:51:06 -0300

    concertando bug do file

--- data/utils.js
+++ data/utils.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const fs = require('fs-extra');
-const { json } = require('path');
+const { join } = require('path');
 
-const loadSQLQueries = async (fileName) => {
-    const filePath = json(process.cwd(), 'data', fileName);
-    const file = fs.readdir(filePath);
-    const sqlFiles = await file.filter( f => f.endsWith('.sql'));
+
+const loadSQLQueries = async (folderName) => {
+    const filePath = join(process.cwd(), 'data', folderName);
+    const files = await fs.readdir(filePath);
+    const sqlFiles = await files.filter(f => f.endsWith('.sql'));
     const queries = {}
-
-    for(const slqfile of sqlFiles){
-        const query = await fs.readFileSync(join(filePath, sqlFiles), { encoding: 'UTF-8'});
-        queries[slqfile.replace(".sql", "")] = query
+    for(const sqlfile of sqlFiles){
+        const query = await fs.readFileSync(join(filePath, sqlfile), { encoding:"UTF-8"});
+        queries[sqlfile.replace(".sql","")] = query
     }
     return queries;
 
