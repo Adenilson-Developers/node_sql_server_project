01cb818f2b03548ff76140c23bc5def317d3bd81
Adenilson-Developers <adenilson.developers@gmail.com>
2023-01-02 17:43:39 -0300

    adicionando metodo por id no router

--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -3,9 +3,10 @@ const express = require('express');
 const eventController = require('../controller/eventController');
 const router = express.Router();
 
-const { getEvents } = eventController;
+const { getEvents, getEventId } = eventController;
 
 router.get('/events', getEvents);
+router.get('/event/:id', getEventId);
 
 module.exports = {
     routes: router
