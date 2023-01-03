bd23af834721a63cac944164b04622a9e9380349
Adenilson-Developers <adenilson.developers@gmail.com>
2023-01-03 20:54:45 -0300

    criando metodo crateEvent

--- data/events/index.js
+++ data/events/index.js
@@ -12,15 +12,15 @@ const getEvents = async () => {
         return list.recordset;
     } catch(error){
         return error.message;
-    }
-}
+    } 
+} 
 
 const getEventId = async (eventId) => {
     try{
         let pool = await sql.connect(config.sql);
         const sqlQueries = await utils.loadSQLQueries('events');
-        const oneEvent = pool.require()
-                             .input('eventId', sql.int, eventId)
+        const oneEvent = await pool.request()
+                             .input('eventId', sql.Int, eventId)
                              .query(sqlQueries.eventById)
         return oneEvent.recordset;
     }catch(error){
@@ -28,7 +28,27 @@ const getEventId = async (eventId) => {
     }
 }
 
+const createEvent = async (eventData) => {
+    try{
+
+        let pool = await sql.connect(config.sql);
+        const sqlQueries = await utils.loadSQLQueries('events');
+        const insertEvent = pool.request()
+                               .input('eventTitle', sql.NVarChar(100), eventData.eventTitle)
+                               .input('eventDescription', sql.NVarChar(1500), eventData.eventDescription)
+                               .input('startDate', sql.Date, eventData.startDate)
+                               .input('endDate', sql.Date, eventData.endDate)
+                               .input('evenue', sql.NVarChar(200), eventData.evenue)
+                               .input('maxMembers', sql.NVarChar, eventData.maxMembers)
+                               .query(sqlQueries.createEvent)
+        return insertEvent.recordset;
+    }catch(error){
+        return error.message;
+    }
+}
+
 module.exports = {
     getEvents,
-    getEventId
+    getEventId,
+    createEvent
 }
