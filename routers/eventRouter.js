e07882bb0aa1b6b2f05693755e76ab5048f82549
Adenilson-Developers <adenilson.developers@gmail.com>
2023-01-03 21:10:17 -0300

    passando o metodo add no router

--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -3,10 +3,11 @@ const express = require('express');
 const eventController = require('../controller/eventController');
 const router = express.Router();
 
-const { getEvents, getEventId } = eventController;
+const { getEvents, getEventId, addEvent } = eventController;
 
 router.get('/events', getEvents);
 router.get('/event/:id', getEventId);
+router.post('/event', addEvent);
 
 module.exports = {
     routes: router
