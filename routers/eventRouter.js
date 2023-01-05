2a0acb2760d44a31a5a71369b467cdc2a6bd44af
Adenilson-Developers <adenilson.developers@gmail.com>
2023-01-04 23:43:32 -0300

    adicionando o metodo update no router

--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -3,11 +3,12 @@ const express = require('express');
 const eventController = require('../controller/eventController');
 const router = express.Router();
 
-const { getEvents, getEventId, addEvent } = eventController;
+const { getEvents, getEventId, addEvent, updateEvent } = eventController;
 
 router.get('/events', getEvents);
 router.get('/event/:id', getEventId);
 router.post('/event', addEvent); 
+router.put('/event/:id', updateEvent)
 
 module.exports = { 
     routes: router
